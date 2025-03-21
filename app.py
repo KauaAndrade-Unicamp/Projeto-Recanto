65369143ba3aaeb824073c2b1a7861fb965bcbe2
KauaAndrade-Unicamp <kauaestudosprog@gmail.com>
2025-03-21 19:56:33 -0300

    atualizações

--- app.py
+++ app.py
@@ -2,28 +2,31 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
-from flask_migrate import Migrate
+from flask_migrate import Migrate 
 
 load_dotenv()
 
 app = Flask(__name__)
 
 database_url = os.getenv('DATABASE_URL')
+
 if not database_url:
     raise ValueError("A variável de ambiente DATABASE_URL não está definida.")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Corrigido aqui
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+    
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)  # ✅ Agora está correto
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     senha = db.Column(db.String(120), nullable=False)
     nome = db.Column(db.String(120))
-
-migrate = Migrate(app, db)
 
 @app.route('/')
 def getpalavra():
